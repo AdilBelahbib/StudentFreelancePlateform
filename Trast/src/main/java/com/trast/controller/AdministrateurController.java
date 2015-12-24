a13174c08ae8d19a6b3b8856d75c3f9071ab31b7
AdilBelahbib <adil.belahbib@gmail.com>
2015-12-24 11:21:00 +0000

    Administrator: accepting sign ups

--- Trast/src/main/java/com/trast/controller/AdministrateurController.java
+++ Trast/src/main/java/com/trast/controller/AdministrateurController.java
@@ -1,12 +1,20 @@
 package com.trast.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.trast.dao.EntrepriseDAO;
 import com.trast.model.Administrateur;
+import com.trast.model.Entreprise;
+import com.trast.model.EtatCompte;
 
 @ManagedBean(name = "administrateurController", eager = true)
 @SessionScoped
@@ -16,6 +24,16 @@ public class AdministrateurController implements Serializable {
 	
 	@ManagedProperty(value = "#{adminitrateur}")
 	private Administrateur admin;
+	private List<Entreprise> entreprises;
+	private Entreprise entreprise;
+	
+	public Entreprise getEntreprise() {
+		return entreprise;
+	}
+
+	public void setEntreprise(Entreprise entreprise) {
+		this.entreprise = entreprise;
+	}
 
 	public Administrateur getAdmin() {
 		return admin;
@@ -24,6 +42,49 @@ public class AdministrateurController implements Serializable {
 	public void setAdmin(Administrateur admin) {
 		this.admin = admin;
 	}
-
 	
+	public List<Entreprise> getEntreprises() {
+		return entreprises;
+	}
+
+	public void setEntreprises(List<Entreprise> entreprises) {
+		this.entreprises = entreprises;
+	}
+
+	public String getEntreprisesEnAttente()
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		EntrepriseDAO entrepriseDAO = (EntrepriseDAO) context.getBean("entrepriseDao");
+		entreprises = entrepriseDAO.getEntreprisesByEtatCompte(EtatCompte.EN_ATTENTE);
+		((ConfigurableApplicationContext)context).close();
+		
+		return "/views/admin/listedemandesinscriptions.xhtml";
+	}
+	
+	public void accepterEntreprise()
+	{
+		System.out.println("ACCEPTER");
+		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		EntrepriseDAO entrepriseDAO = (EntrepriseDAO) context.getBean("entrepriseDao");
+		
+		//Mettre à jour l'état de l'entreprise
+		entreprise.setEtatCompte(EtatCompte.ACTIVE);
+		entrepriseDAO.modifierEntreprise(entreprise);
+		((ConfigurableApplicationContext)context).close();
+		
+		entreprises.remove(entreprise);
+	}
+	
+	public void refuserEntreprise()
+	{
+		System.out.println("REFUSER");
+		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		EntrepriseDAO entrepriseDAO = (EntrepriseDAO) context.getBean("entrepriseDao");
+		
+		//Supprimer l'entreprise de la bdd
+		entrepriseDAO.supprimerEntreprise(entreprise.getId());
+		((ConfigurableApplicationContext)context).close();
+		
+		entreprises.remove(entreprise);
+	}
 }
